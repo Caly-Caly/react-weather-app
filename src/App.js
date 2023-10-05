51381555d245d51a515973d45c754b84c0cb027b
Caly-Caly <calystaaponte+github@gmail.com>
2023-10-04 21:38:37 -0400

    Added Github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
-};
+  return (
+    <div className="App">
+      {" "}
+      Weather App
+      <a href="https://github.com/Caly-Caly/react-weather-app">Public Source</a>
+    </div>
+  );
+}
