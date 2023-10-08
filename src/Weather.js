f8cdb6712a83afe4221738e95e13a9fe97a061d8
Caly-Caly <calystaaponte+github@gmail.com>
2023-10-08 00:00:24 -0400

    implemented useState and added real time weather data to our interface

--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
+
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
-              autoFocus="on"
-            />
+  const [weatherData, setweatherData] = useState({ ready: false });
+  function showData(response) {
+    console.log(response.data);
+    setweatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      conditions: response.data.condition.description,
+      feels_like: response.data.temperature.feels_like,
+      iconUrl: response.data.condition.icon_url,
+      iconInfo: response.data.condition.icon,
+    });
+
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul className="timeDateInfo">
+          <li>Friday 07:36</li>
+          <li>{weatherData.conditions}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <img src={weatherData.iconUrl} alt={weatherData.iconInfo} />
+            <span className="temp">{Math.round(weatherData.temperature)}</span>
+            <span className="unit">°C</span>
           </div>
-        </div>
-      </form>
-      <h1>Brooklyn</h1>
-      <ul className="timeDateInfo">
-        <li>Friday 07:36</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-day.png" />
-          <span className="temp">9</span>
-          <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 18%</li>
-            <li>Humidity: 96%</li>
-            <li>Wind Speed: 5pm</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Feels Like: {Math.round(weatherData.feels_like)}°C</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind Speed: {Math.round(weatherData.wind)}mph</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "744441eb32ea7ceo3fb901c610f1d4t9";
+    let city = "Brooklyn";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showData);
+    return "Loading...";
+  }
 }
