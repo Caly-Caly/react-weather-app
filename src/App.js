87b97e145118949219c982b18e92e0a0cd0646ff
Caly-Caly <calystaaponte+github@gmail.com>
2023-10-07 22:32:17 -0400

    Created the interface of the weather app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
-
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      Weather App
-      <br />
-      <a href="https://github.com/Caly-Caly/react-weather-app">Public Source</a>
+      <div className="container">
+        {" "}
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          {" "}
+          This project was coded by Calysta Aponte and is{" "}
+          <a
+            href="https://github.com/Caly-Caly/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
