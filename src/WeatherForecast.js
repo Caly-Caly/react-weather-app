3f83cba3a22f8a0f0977053593fb804a21a461ad
Caly-Caly <calystaaponte+github@gmail.com>
2023-11-05 23:29:01 -0500

    Fixed up the coords api call and created a function for it

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,15 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function loadUp() {
+    let apiKey = "744441eb32ea7ceo3fb901c610f1d4t9";
+    let lati = props.coordinates.latitude;
+    let long = props.coordinates.longitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${long}&lat=${lati}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -35,12 +44,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "744441eb32ea7ceo3fb901c610f1d4t9";
-    let lati = props.coordinates.latitude;
-    let long = props.coordinates.longitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${long}&lat=${lati}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
+    loadUp();
 
     return null;
   }
